42abd75cc660e98e5481ee1470de3c27cb91256f
Dennis Phetsomphou <dennisph@my.yorku.ca>
2022-06-02 21:28:31 -0400

    feat: navbar

--- portfolio-app/src/app/landing/landing.component.ts
+++ portfolio-app/src/app/landing/landing.component.ts
@@ -9,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class LandingComponent implements OnInit {
   path: string = "../../assets/images/selfie.png";
   logo: string = "../../assets/images/logo.png";
-  
+  arrow: string = "../../assets/images/icons/arrow2.png";
+
   constructor() { }
 
   ngOnInit(): void {
